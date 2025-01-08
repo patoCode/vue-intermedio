cb624b2f6a20fae03d3c06eeea8d25088dc4c388
denis.rodriguez <denis.rodriguez@endesyc.bo>
2025-01-08 15:41:17 -0400

    Http Request/Axios(Interfaces/Promise.all(): Creacion de interfaces para la recepcion de datos desde un servicio REST. Archivo independiente para la llamada del REST. Promise.all() Metodo para resolver varias promesas

---
+++ src/pokemons/helpers/get-pokemons.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import pokemonApi from '../api/pokemonApi'
+import type { Pokemon, PokemonListResponse, PokemonResponse } from '../interfaces'
+
+export const getPokemons = async (): Promise<Pokemon[]> => {
+  const { data } = await pokemonApi.get<PokemonListResponse>('pokemon?limit=45')
+
+  const pokemonPromises: Promise<Pokemon>[] = []
+  for (const { url } of data.results) {
+    const pokemonPromise = axios.get<PokemonResponse>(url).then(({ data }) => {
+      return {
+        id: data.id,
+        name: data.name,
+        fronSprite: data.sprites.front_default,
+      }
+    })
+    pokemonPromises.push(pokemonPromise)
+  }
+  const pokemons = await Promise.all(pokemonPromises)
+  return pokemons
+}
--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,4 +1,9 @@
 <template>
   <div>Pokemon List</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getPokemons } from '../helpers/get-pokemons'
+const pokemons = getPokemons()
+
+console.log('Pokemons', pokemons)
+</script>
